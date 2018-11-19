a329b434b90b46d96dbab37da5045ae8b4813fb7
Rodrigo Stuchi <rod.stuchi@gmail.com>
2018-11-19 02:01:53 -0200

    fix rofi-dmenu emoji

--- .scripts/emoji/rofi-emoji.sh
+++ .scripts/emoji/rofi-emoji.sh
@@ -1,4 +1,12 @@
 #! /bin/sh
 
-rg -v "#$" ~/.scripts/emoji/emojis | rofi -i -matching fuzzy -dmenu | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
+rg -v "#$" ~/.scripts/emoji/emojis \
+  | rofi -i -matching fuzzy -dmenu -location 2 \
+    -no-disable-history -sort -levenshtein-sort \
+    -dpi 130 -width 40 -lines 30 -multi-select \
+    -theme ~/.config/rofi/Arc.rasi \
+  | awk '{print $1}' \
+  | tr -d '\n' \
+  | xclip -selection clipboard
 pgrep -x dunst >/dev/null && notify-send "$(xclip -o -selection clipboard) was copied."
+
